8780e180ba38e136e798955f85317b2955719890
Amar Puri <41878077+puriamar@users.noreply.github.com>
2024-07-04 23:09:41 -0700

    Few css changes

--- src/components/quiz.jsx
+++ src/components/quiz.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 
 function Quiz() {
   const [currQuestion, setCurrentQuestion] = useState(0);
+  const [selectedAnswers, setSelectedAnswers] = useState({});
 
   const nextQuestion = () => {
     if (currQuestion === questions.length - 1) {
@@ -17,18 +18,32 @@ function Quiz() {
     setCurrentQuestion(currQuestion - 1);
   };
 
+  const handleOptionSelect = (option) => {
+    setSelectedAnswers({
+      ...selectedAnswers,
+      [currQuestion]: option,
+    });
+  };
+
+  const cleaarSelectedAnswers = () => {
+    setSelectedAnswers({});
+  };
+
   const questions = [
     {
       question: "First Question",
-      option: ["option 1", "option 2", "option 3", "option 4"],
+      options: ["option 1 sadasda asda", "option 2", "option 3", "option 4"],
+      answer: "option 1",
     },
     {
-      question: "First Question 2",
-      option: ["option a", "option b", "option c", "option d"],
+      question: "Second Question",
+      options: ["option a", "option b", "option c", "option d"],
+      answer: "option a",
     },
     {
-      question: "First Question 3",
-      option: ["option ab", "option bc", "option cd", "option ed"],
+      question: "Third Question",
+      options: ["option ab", "option bc", "option cd", "option ed"],
+      answer: "option ab",
     },
   ];
 
@@ -37,14 +52,66 @@ function Quiz() {
       <p>
         {currQuestion + 1}: {questions[currQuestion].question}
       </p>
-      <ul>
-        {questions[currQuestion].option.map((option, index) => (
-          <li key={index}>{option}</li>
+      <div>
+        {questions[currQuestion].options.map((option, index) => (
+          <button
+            key={index}
+            onClick={() => handleOptionSelect(option)}
+            style={{
+              width: "400px",
+              margin: "5px",
+              color: "black",
+              backgroundColor:
+                selectedAnswers[currQuestion] === option
+                  ? "lightblue"
+                  : "white",
+            }}
+          >
+            {option}
+          </button>
         ))}
-      </ul>
+      </div>
+      <br />
+      <button
+        style={{
+          margin: "5px",
+        }}
+        onClick={prevQuestion}
+      >
+        Prev
+      </button>
+      <button
+        style={{
+          margin: "5px",
+        }}
+        onClick={nextQuestion}
+      >
+        Next
+      </button>
 
-      <button onClick={prevQuestion}>Prev</button>
-      <button onClick={nextQuestion}>Next</button>
+      <div>
+        <h3>
+          {selectedAnswers[currQuestion]
+            ? selectedAnswers[currQuestion] === questions[currQuestion].answer
+              ? "Yayyy!!! Amu also selected the same option."
+              : "Ohnoo!!! Amu selected different option."
+            : "Select an option to see Amu's answer."}
+        </h3>
+        <h3>
+          {selectedAnswers[currQuestion]
+            ? "Amu's choice: " + questions[currQuestion].answer
+            : ""}
+        </h3>
+      </div>
+
+      <button
+        style={{
+          margin: "5px",
+        }}
+        onClick={cleaarSelectedAnswers}
+      >
+        Clear Selected Answers
+      </button>
     </div>
   );
 }
