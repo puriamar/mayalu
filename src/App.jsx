3264c58e3e394ef326e1ed4c4c9bb99e7c6ec20c
Amar Puri <41878077+puriamar@users.noreply.github.com>
2024-07-04 18:21:52 -0700

    DEPLOY CONFIG.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import sampleLogo from "./assets/sample.svg";
 import "./App.css";
 import Countdown from "./components/countdown";
 
@@ -8,16 +7,6 @@ function App() {
   return (
     <>
       <div>
-        <a
-          style={{ fontSize: "0.1em", marginRight: "0.5em" }}
-          href="https://www.freepik.com/free-photo/funny-german-shepherd-dog-3d-illustration_13880097.htm#fromView=search&page=1&position=7&uuid=4444a24b-693b-48b7-a98e-848ba92a5b11"
-        >
-          Image by julos on Freepik
-        </a>
-        <a target="_blank">
-          <img src={sampleLogo} className="logo react" alt="React logo" />
-        </a>
-
         <Countdown targetDate={targetDate} />
       </div>
     </>
